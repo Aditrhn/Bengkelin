d21aec0ebdaa139d7c8d8b812fe3598749994848
Makrovic <44518143+Makrovic@users.noreply.github.com>
2019-10-02 05:09:45 +0700

    Initial commit

---
+++ app/src/main/java/com/bklndev/bengkelin_prototype2/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.bklndev.bengkelin_prototype2
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bklndev.bengkelin_prototype2.fragment.AccountFragment
+import com.bklndev.bengkelin_prototype2.fragment.ChatFragment
+import com.bklndev.bengkelin_prototype2.fragment.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val manager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createFragmentHome()
+
+        dash_nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.dash_nav_home -> {
+                    createFragmentHome()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.dash_nav_chat -> {
+                    createFragmentChat()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.dash_nav_account -> {
+                    createFragmentAccount()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+    fun createFragmentHome(){
+        val transaction = manager.beginTransaction()
+        val fragment = HomeFragment()
+        transaction.replace(R.id.dash_container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    fun createFragmentChat(){
+        val transaction = manager.beginTransaction()
+        val fragment = ChatFragment()
+        transaction.replace(R.id.dash_container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    fun createFragmentAccount(){
+        val transaction = manager.beginTransaction()
+        val fragment = AccountFragment()
+        transaction.replace(R.id.dash_container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
